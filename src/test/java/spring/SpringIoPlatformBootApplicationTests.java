cdd402f5cd96f8e203f5195757df9e90da11acc1
Sejin Kim <sejila@insightinfo.kr>
2015-08-13 10:55:30 +0900

    first commit

---
+++ src/test/java/spring/SpringIoPlatformBootApplicationTests.java
@@ -0,0 +1,16 @@
+package spring;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = SpringIoPlatformBootApplication.class)
+public class SpringIoPlatformBootApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
